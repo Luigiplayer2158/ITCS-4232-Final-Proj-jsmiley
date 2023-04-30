ca119263bf6944978b20c7a79d2713a2ee977eec
Luigiplayer2158 <jps.shs.school@gmail.com>
2023-04-30 01:39:37 -0400

    death and hud

---
+++ FinalProjectCapstone/HUD.gd
@@ -0,0 +1,35 @@
+extends CanvasLayer
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+onready var deathCount = 0
+
+onready var player = $"../Player/MovingPlayer"
+
+onready var anim = $ColorRect/AnimationPlayer
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+
+	anim.get_animation("fadeOut").set_loop(false)
+	anim.get_animation("fadeIn").set_loop(false)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func updateBoxes(boxCount):
+	$boxes.text = "x " + str(boxCount)
+	
+
+func _on_Area_body_entered(body):
+	if body is KinematicBody:
+		print("it has occured");
+		deathCount = deathCount + 1
+		$deaths.text = "x " + str(deathCount)
+		get_tree().paused = true
+		anim.play("fadeOut")
+		player.global_transform.origin = Vector3(0.00,5.00,0.00)
+		get_tree().paused = false
+		anim.play("fadeIn")
+		
